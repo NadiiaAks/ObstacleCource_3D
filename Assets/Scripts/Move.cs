f39371767c27bd6406bf7907dfad6f5e76f994f6
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-08-17 15:45:45 +0300

    Change color of wall, if it hits object

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    [SerializeField] float xValue = 0.01f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0f;
+    [SerializeField] float moveSpeed = 3f;
     void Start()
     {
 
@@ -14,6 +12,13 @@ public class Move : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0f, zValue);
     }
 }
